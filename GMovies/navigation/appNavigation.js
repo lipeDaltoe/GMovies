84d84209f664fa46d9708bae2cdf76533b90ae64
lipeDaltoe <felipedaltoe9@gmail.com>
2024-11-25 11:22:27 -0300

    Finalizado

--- GMovies/navigation/appNavigation.js
+++ GMovies/navigation/appNavigation.js
@@ -1,5 +1,5 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import MovieScreen from '../screens/MovieScreen';
 import PersonScreen from '../screens/PersonScreen';
@@ -10,12 +10,12 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name='Movie' component={MovieScreen} options={{headerShown: false}}/>
-            <Stack.Screen name='Person' component={PersonScreen} options={{headerShown: false}}/>
-            <Stack.Screen name='Search' component={SearchScreen} options={{headerShown: false}}/>
-        </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' options={{ headerShown: false }} component={HomeScreen} />
+        <Stack.Screen name='Movie' options={{ headerShown: false }} component={MovieScreen} />
+        <Stack.Screen name='Person' options={{ headerShown: false }} component={PersonScreen} />
+        <Stack.Screen name='Search' options={{ headerShown: false }} component={SearchScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
--- GMovies/screens/HomeScreen.js
+++ GMovies/screens/HomeScreen.js
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
-import { View, Text, Platform, TouchableOpacity, ScrollView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useEffect, useState } from 'react'
+import { View, Text, Platform, TouchableOpacity, ScrollView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Bars3BottomLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 import { styles } from '../theme';
+import { useNavigation } from '@react-navigation/native'
 import TrendingMovies from '../components/trendingMovies';
 import MovieList from '../components/movieList';
+import Loading from '../components/loading';
+import { fetchTopRatedMovies, fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb';
 
 const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
-  const [trending, setTrending] = useState([1,2,3]);
-  const [upcoming, setUpcoming] = useState([1,2,3]);
-  const [topRated, setTopRated] = useState([1,2,3]);
+  const [trending, setTrending] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    getTrendingMovies();
+    getUpcomingMovies();
+    getTopRatedMovies();
+    setLoading(false);
+  }, [])
+
+  const getTrendingMovies = async () => {
+    const data = await fetchTrendingMovies();
+    if (data && data.results) setTrending(data.results);
+  }
+
+  const getUpcomingMovies = async () => {
+    const data = await fetchUpcomingMovies();
+    if (data && data.results) setUpcoming(data.results);
+  }
+
+  const getTopRatedMovies = async () => {
+    const data = await fetchTopRatedMovies();
+    if (data && data.results) setTopRated(data.results);
+  }
+
   return (
     <View className="flex-1 bg-neutral-800">
       <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
-        <StatusBar style='light' />
+        <StatusBar style="light" />
         <View className="flex-row justify-between items-center mx-4 mt-2">
           <TouchableOpacity>
             <Bars3BottomLeftIcon size={30} strokeWidth={2} color="white" />
@@ -24,22 +52,29 @@ export default function HomeScreen() {
           <Text className="text-white text-3xl font-bold">
             <Text style={styles.text}>GM</Text>ovies
           </Text>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('Search')}>
             <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
           </TouchableOpacity>
         </View>
       </SafeAreaView>
 
-      <ScrollView
-        showVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 10 }}
-      >
-        <TrendingMovies data={trending} />
-        
-        <MovieList title="Próximos lançamentos" data={upcoming} />
+      {
+        loading ? (
+          <Loading />
+        ) : (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 10 }}
+          >
+            { trending.length > 0 && <TrendingMovies data={trending} />}
 
-        <MovieList title="Melhores Avaliados" data={topRated} />
-      </ScrollView>
+            <MovieList title="Próximos lançamentos" data={upcoming} />
+
+            <MovieList title="Melhor Avaliados" data={topRated} />
+          </ScrollView>
+        )
+      }
+
     </View>
   )
 }
